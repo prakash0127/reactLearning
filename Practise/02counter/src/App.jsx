fcf84c2a58e81e625f55c25063ac9eeebed2cfee
Prakash gupta <prakashgup50@gmail.com>
2024-03-28 01:26:33 +0530

    revision complete

--- Practise/02counter/src/App.jsx
+++ Practise/02counter/src/App.jsx
@@ -2,22 +2,42 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(5); // in useState(default value it use to take)
+  const [count, setCount] = useState(8); // in useState(default value it use to take)
   // you can give boolean value , number, string, etc
   // and setCounter is a function/method; and use to conrol the counter variable
   // no need to write the name setCounter you can keep anything
-  // let counterValue = 5;
 
-  const removeValue = () => {
-    setCount(count - 1);
-    if (count === 0) {
+  //
+
+  const addValue = () => {
+    // setCount(count + 1);
+    // if you do the same thing like
+    // setCount(count - 1);
+    // setCount(count - 1);
+    // setCount(count - 1);
+
+    // it will add only 1
+
+    // to handle it like want to add 3 time we need precounter any name we take
+
+    setCount((prevCounter) => prevCounter + 1);
+    setCount((prevCounter) => prevCounter + 1);
+    setCount((prevCounter) => prevCounter + 1);
+    setCount((prevCounter) => prevCounter + 1);
+
+    // now if you will click it will add 4 times at same time
+    if (count === 20) {
       setCount(count);
     }
   };
 
-  const addValue = () => {
-    setCount(count + 1);
-    if (count === 20) {
+  const removeValue = () => {
+    // setCount(count - 1);
+    setCount((prevCounter) => prevCounter - 1);
+    setCount((prevCounter) => prevCounter - 1);
+    setCount((prevCounter) => prevCounter - 1);
+    setCount((prevCounter) => prevCounter - 1);
+    if (count === 0) {
       setCount(count);
     }
   };
---
+++ Practise/03tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind Test!
+      </h1>
+      <Card name="Chai" btnText="click me" />
+      <Card name="Code" btnText="info" />
+    </>
+  );
+}
+
+export default App;

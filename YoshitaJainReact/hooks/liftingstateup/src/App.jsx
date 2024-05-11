9c530147d8faf693dd06b6b86b8af55c61153248
Prakash gupta <prakashgup50@gmail.com>
2024-05-12 01:42:44 +0530

    Lifting state up

--- YoshitaJainReact/hooks/liftingstateup/src/App.jsx
+++ YoshitaJainReact/hooks/liftingstateup/src/App.jsx
@@ -5,7 +5,11 @@ import Child from "./Child";
 
 function App() {
   let [firstName, setFirstName] = useState("");
+  // In our app we have created a function that is getData
+  // and we are passing in the function as props to the Child as we can see <Child getData={getData} />
+  // and in the get data we are passing a param that is accepting a data
 
+  //
   function getData(data) {
     console.log(data);
     setFirstName((firstName = data));
@@ -14,7 +18,7 @@ function App() {
   return (
     <>
       {firstName} <br />
-      <h1>Shifting state up </h1>
+      <h1>Lifting-state up </h1>
       <Child getData={getData} />
     </>
   );
--- YoshitaJainReact/hooks/liftingstateup/src/Child.jsx
+++ YoshitaJainReact/hooks/liftingstateup/src/Child.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import { useState } from "react";
 
 function Child(props) {
+  // scenario: we want to pass the data from the Chil component to paraent
+  // means this Child component to App component
+  // here we are taking the input from the form input text and passing it to App component
   const [name, setName] = useState("");
+  // created a state using the useState
 
+  // created a form and from input text we are taking input
   function handleSubmit(e) {
+    // preventing from the default
     e.preventDefault();
+    // here with props we are calling our getDate functionand passing the our Child State
+    // that we have assign the value of our input
     props.getData(name);
   }
-
+  // handle function to get the value and update it to the name state
   function handleChange(e) {
     setName(e.target.value);
   }
+  // added a button and in the form we handle while the submit
   return (
     <div>
       <form action="" onSubmit={handleSubmit}>

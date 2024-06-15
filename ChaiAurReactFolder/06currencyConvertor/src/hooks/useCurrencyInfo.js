e7b967bd384eaf77e21d23ce6a3c3605b125d4cc
Prakash gupta <prakashgup50@gmail.com>
2024-06-15 14:14:04 +0530

    currency project completed

--- ChaiAurReactFolder/06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ ChaiAurReactFolder/06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -8,5 +8,23 @@
 // import { useEffect, useState } from "react";
 
 // function useCurrencyInfo(currency) {
-//   // why we are using "use" because standard value to add in the starting
+//   // why we are using "use" because standard value to add in the starting in react to look luike a hook
 // }
+
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
